b93bfe2316bdfb125cf0ee29ad188093e4acfb9b
Oğulcan Özdemir <ogulcan.ozdemir94@gmail.com>
2018-07-14 14:37:46 +0300

    Bug fixing

---
+++ db_constants.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['servername'] = "localhost";
+$GLOBALS['username'] = 'root';
+$GLOBALS['password'] = 'your_password';
+$GLOBALS['dbname'] = "accounts_database";
+?>
---
+++ make_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+$servername = $GLOBALS['servername'];
+$username = $GLOBALS['username'];
+$password = $GLOBALS['password'];
+$dbname = $GLOBALS['dbname'] ;
+$con = mysqli_connect($servername, $username, $password);
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sql_database = "CREATE DATABASE IF NOT EXISTS accounts_database";
+if (mysqli_query($con, $sql_database)) {
+    // echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($con);
+}
+
+
+$con_table= mysqli_connect($servername, $username, $password, $dbname);
+$sql_table = "CREATE TABLE IF NOT EXISTS Accounts (
+id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(30) NOT NULL,
+password VARCHAR (10) NOT NULL
+)";
+
+if (mysqli_query($con_table, $sql_table)) {
+    // echo "Table Accounts created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($con_table);
+}
+
+mysqli_close($con);
+mysqli_close($con_table);
+?>
